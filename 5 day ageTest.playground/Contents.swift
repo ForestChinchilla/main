d26e7c37bb689789850ed2b13fee14ca5434e984
ForestChinchilla <forestchinchilla@mail.ru>
2026-05-16 10:44:28 +0300

    ageTest добавил

---
+++ 5 day ageTest.playground/Contents.swift
@@ -0,0 +1,13 @@
+func ageTest (name: String, age:Int) {
+    if age >= 18 {
+        return print("Hey, \(name) let's do this")
+    }
+    else{
+        return print("Oh no, \(name) is only \(age), you are too young for this shit")
+    }
+}
+
+ageTest (name: "Vasya", age: 18)
+ageTest (name: "Kira", age: 14)
+
+
